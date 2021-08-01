fef500122bfef9b231c421465f96c41bbd5d439a
tran quang huy <ieltsaddict@gmail.com>
2021-08-01 09:43:14 +0700

    first add

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Menu from './components/Menu';
-import routes from './routes';
-import './App.css';
+import React from 'react'
 
-
-class App extends Component { 
-    render() {
-        return (
-           <Router>
-                <div className='App'>
-                    {/* Menuu */}
-                    <Menu></Menu>
-                    {/* Noi dung */}
-                    <Switch>
-                        { this.showContentMenu(routes) }
-                    </Switch>
-                </div>
-           </Router>
-        );
-    }
-
-    showContentMenu = (routes) => {
-        var result = null;
-        if (routes.length > 0) {
-            result = routes.map((route, index) => {
-                return(
-                    <Route 
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.main}
-                        key={index}
-                    ></Route>
-                );
-            })
-        }
-        return result;
-    }
+const App = () => {
+    return (
+        <div>
+            xin chao tat ca cac ban
+        </div>
+    )
 }
-export default (App);
- 
+
+export default App
